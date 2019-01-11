6adcb655dace269720015b0c3d9574173f0f0fb7
Tim Lin <tony@widelab.org>
2019-01-11 19:22:06 +0800

    Update response using queries from TCP server

--- externalAPI/main.go
+++ externalAPI/main.go
@@ -5,25 +5,32 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"time"
+)
+
+const (
+	port = "8888"
 )
 
 func main() {
 	// connectionCount := 1
 
-	l, err := net.Listen("tcp", ":8888")
+	l, err := net.Listen("tcp", ":"+port)
 
 	if err != nil {
 		log.Fatalf("Listen: %v", err)
 	}
 
+	log.Println("Start listening port:" + port)
+
 	defer l.Close()
 
 	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		// time.Sleep(3 * time.Second)
-		fmt.Fprintf(w, "Welcome to my website!")
+		if sliceQuery, ok := r.URL.Query()["q"]; ok {
+			time.Sleep(1 * time.Second)
+			fmt.Fprintf(w, "External api response query: "+sliceQuery[0])
+		}
 	})
-
-	// l = netutil.LimitListener(l, connectionCount)
 
 	log.Fatal(http.Serve(l, nil))
 }
